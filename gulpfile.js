1b329970d0dd9e48a3454fa7469bd93ab38479da
Johan Uddståhl <baktakt@gmail.com>
2017-05-17 12:52:55 +0200

    minify css

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,14 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');  
 var rename = require('gulp-rename');  
 var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
 
 //script paths
 var jsFiles = 'scripts/**/*.js',  
-    jsDest = 'dist/scripts';
+    jsDest = 'dist/scripts',
+    cssFiles = 'styles/**/*.css',
+    cssDest = 'dist/css';
 
 gulp.task('scripts', function() {  
     return gulp.src(jsFiles)
@@ -15,3 +19,15 @@ gulp.task('scripts', function() {
         .pipe(uglify())
         .pipe(gulp.dest(jsDest));
 });
+
+gulp.task('styles', function() {  
+    
+    return gulp.src(cssFiles)
+        .pipe(minifyCSS())
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(concat('visualizer.min.css'))
+        .pipe(gulp.dest(cssDest))
+});
+
+
+
